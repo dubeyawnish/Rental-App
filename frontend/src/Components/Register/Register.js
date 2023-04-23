7911df251700c20fee4d6c3e4a89294ad741d03f
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-23 19:54:24 +0530

    Register page initailized

--- frontend/src/Components/Register/Register.js
+++ frontend/src/Components/Register/Register.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import  './Register.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <div className='container mt-3  shadow p-3 mb-5 bg-body-tertiary rounded'>
+        <h3 className='text-center text-uppercase'>Register User</h3>
+      </div>
+
+      <div className='container mb-4'>
+        <div className="contact-form-container mx-auto text-muted shadow p-3 lh-2" >
+
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
