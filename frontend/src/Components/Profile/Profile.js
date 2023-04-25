3fd4850b60dc18fd867432c35e2455d03d3baa5c
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-26 00:21:38 +0530

    right section of profile page done

--- frontend/src/Components/Profile/Profile.js
+++ frontend/src/Components/Profile/Profile.js
@@ -10,22 +10,121 @@ const Profile = () => {
             first column
 
           </div>
-          <div className='col-lg-8 col-md-8 col-sm-12 border border-danger'>
-            
+          <div className='col-lg-8 col-md-8 col-sm-12 '>
+            <div className="border">
               <div className='d-flex justify-content-between'>
                 <div className=''>
-                  <h5 className='mt-2'>Personal Details</h5>
+                  <h5 className='mt-3 ms-3'>Personal Details</h5>
                 </div>
+                <div className='me-3'>
+                  <button type='button' className=' mt-2 btn btn-warning'><i class="fa-regular fa-pen-to-square"></i> Edit</button>
+                </div>
+
+              </div>
+              <hr className='mx-3' />
+              <div className='d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className=' text-muted'>FullName</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>Owner name</h6>
+
+                </div>
+              </div>
+              <hr className='mx-3' />
+              <div className='d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className='text-muted'>Email</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>Owner@name.com</h6>
+
+                </div>
+              </div>
+              <hr className='mx-3' />
+              <div className='d-flex mb-3'>
+                <div className='ms-3 me-5' >
+                  <h6 className='text-muted'>Phone</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>45612378</h6>
+
+                </div>
+              </div>
+            </div>
+             
+            <div className=" my-4 border">
+              <div className='d-flex justify-content-between'>
                 <div className=''>
-                  <button type='button' className='btn btn-warning'><i class="fa-regular fa-pen-to-square"></i> Edit</button>
+                  <h5 className='mt-3 ms-3'>Address Details</h5>
+                </div>
+                <div className='me-3'>
+                  <button type='button' className=' mt-2 btn btn-warning'><i class="fa-regular fa-pen-to-square"></i> Edit</button>
                 </div>
 
               </div>
-              <hr/>
-              <div>
+              <hr className='mx-3' />
+              <div className='d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className=' text-muted'>Address 1</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>Owner name</h6>
 
+                </div>
               </div>
-           
+              <hr className='mx-3' />
+              <div className='d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className='text-muted'>Address 2</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>Owner@name.com</h6>
+
+                </div>
+              </div>
+              <hr className='mx-3' />
+              <div className='d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className='text-muted'>City</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>45612378</h6>
+
+                </div>
+              </div>
+              <hr className='mx-3' />
+              <div className='d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className='text-muted'>State</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>45612378</h6>
+
+                </div>
+              </div>
+              <hr className='mx-3' />
+              <div className='d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className='text-muted'>Pincode</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>45612378</h6>
+
+                </div>
+              </div>
+              <hr className='mx-3' />
+              <div className='mb-3 d-flex'>
+                <div className='ms-3 me-5' >
+                  <h6 className='text-muted'>Country</h6>
+                </div>
+                <div className='ms-5'>
+                  <h6 className='text-muted'>45612378</h6>
+
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
