5837b2cdac1d1bb11babc36ff356e216b6a05e96
Awnish Dubey <dubeyawnish6@gmail.com>
2023-05-01 16:57:51 +0530

    All property data fetch successfully

--- frontend/src/Components/AllProperty/AllProperty.js
+++ frontend/src/Components/AllProperty/AllProperty.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { API_BASE_URL } from '../../config';
 
 const AllProperty = () => {
+
+  const [properties,setProperties] =useState([]);
+  const [loading,setLoading] =useState(false);
+   
+  const getAllProperty=async()=>{
+    const result=await axios.get(`${API_BASE_URL}/viewAllProperties`);
+    console.log(result);
+    setProperties(result.data.allProperties);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getAllProperty();
+    setLoading(true);
+
+  }, [])
+  
+ 
+
+
   return (
     <div className='container'>
        <div className='container mt-3  shadow p-3 mb-1 bg-body-tertiary rounded'>
                 <h3 className='text-center text-muted text-uppercase'>All Property</h3>
             </div>
-            <h5 className='text-muted mt-2'>All Property:1</h5>
+            <h5 className='text-muted mt-2'>All Property:{properties.length}</h5>
 
             <div className='my-3'>
         <input className=" text-muted mt-3 mb-4  me-2 form-control"  type="search" placeholder="Search Title..." />
 
         <div className='col-lg-4 col-md-4 col-sm-12 mb-3'>
               <div className="card" >
-                <img src='https://images.unsplash.com/photo-1682277001070-fca2cfd671ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' height={300} className="card-img-top" alt="..." />
+                <img src={`${API_BASE_URL}/files/${properties[0].propertyImgName}`} height={300} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h6 className="card-title text-muted">title</h6>
                   <p className="card-text text-muted">description</p>
