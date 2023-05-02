32db15097b44b9163f40d12c1e4d020dd839d92e
Awnish Dubey <dubeyawnish6@gmail.com>
2023-05-02 23:53:26 +0530

    tenant delete functionality from detail property page has been done

--- frontend/src/Components/DetailProperty/DetailProperty.js
+++ frontend/src/Components/DetailProperty/DetailProperty.js
@@ -43,6 +43,12 @@ const DetailProperty = () => {
     const func = () => {
         navigate(-1);// -1 is used for go to previous page 
     }
+    const delTenant = async (tenantId) => {
+        const deleteTenant = await axios.delete(`${API_BASE_URL}/deleteIntrestedTenantById/${tenantId}`, CONFIG_OBJ)
+        if (deleteTenant.status === 200) {
+          getTenantsList(propertyId);
+        }
+      }
 
     return (
         <div className='container'>
@@ -190,7 +196,7 @@ const DetailProperty = () => {
                                     <td>{tenant.user.email}</td>
                                     <td>{tenant.user.phone}</td>
                                     <td><button className='btn btn-sm btn-primary' >Add</button></td>
-                                    <td><button className='btn btn-sm btn-danger' ><i class="fa-solid fa-circle-xmark"></i></button></td>
+                                    <td><button className='btn btn-sm btn-danger' onClick={()=>{delTenant(tenant._id)}} ><i class="fa-solid fa-circle-xmark"></i></button></td>
                                 </tr>)
                             })
                             }
