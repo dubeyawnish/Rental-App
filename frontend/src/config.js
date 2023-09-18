8294b93ce051a126ff48917eabbbec9b1be32b9d
Awnish Dubey <dubeyawnish6@gmail.com>
2023-09-19 00:33:23 +0530

    Api base url channge

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 //export const API_BASE_URL='http://localhost:5000'
-export const API_BASE_URL='https://ec2-16-171-230-7.eu-north-1.compute.amazonaws.com:5000'
+export const API_BASE_URL='http://ec2-3-6-126-223.ap-south-1.compute.amazonaws.com:5000'
